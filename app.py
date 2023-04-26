5fbdcd12e4295c7282a7fa9b094e00affeffab95
Sabuhi <sabuhiabs0@mail.ru>
2023-04-26 17:48:38 +0900

    Latest update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash, session, \
+    send_from_directory, get_flashed_messages
 from my_lib import database_worker, encrypt_password, check_password
 from werkzeug.utils import secure_filename
 from functools import wraps
@@ -8,7 +9,8 @@ import dotenv
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
+app.secret_key = 'you-will-never-guess'
+app.config['UPLOAD_FOLDER'] = 'upload_folder'
 
 dotenv.load_dotenv()
 token_key = os.getenv('TOKEN_ENCRYPTION_KEY')
@@ -35,20 +37,11 @@ def create_database():
     query_user = """CREATE table if not exists users (
         id INTEGER PRIMARY KEY,
         name TEXT,
+        surname TEXT,
         country TEXT,
         username TEXT,
         email TEXT,
         password TEXT
-    )
-    """
-    query_post = """CREATE table if not exists posts(
-        id INTEGER PRIMARY KEY,
-        title VARCHAR(100),
-        category TEXT,
-        timestamp TEXT,
-        content TEXT,
-        user_id INTEGER,
-        FOREIGN KEY (user_id) REFERENCES users(id) on delete cascade
     )
     """
     query_saves = """CREATE table if not exists saves(
@@ -77,11 +70,32 @@ def create_database():
         FOREIGN KEY (followed) REFERENCES users(id) on delete cascade
     )
     """
+    query_recipe = """CREATE table if not exists recipes(
+        id INTEGER PRIMARY KEY,
+        title VARCHAR(100),
+        timestamp TEXT,
+        content TEXT,
+        ingredients TEXT,
+        user_id INTEGER,
+        FOREIGN KEY (user_id) REFERENCES users(id) on delete cascade
+    )
+    """
+    query_restaurant = """CREATE table if not exists reviews(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(100),
+        timestamp TEXT,
+        stars INTEGER,
+        content TEXT,
+        user_id INTEGER,
+        FOREIGN KEY (user_id) REFERENCES users(id) on delete cascade
+    )
+    """
     db.run_save(query_user)
-    db.run_save(query_post)
+    db.run_save(query_recipe)
     db.run_save(query_saves)
     db.run_save(query_comments)
     db.run_save(query_follow)
+    db.run_save(query_restaurant)
     db.close()
 
 @app.route('/')
@@ -89,13 +103,12 @@ def create_database():
 @token_required
 def index():
     data = jwt.decode(session['token'], token_key, algorithms=['HS256'])
-    print(data['user_id'])
     user_id = data['user_id']
     db = database_worker('social_net.db')
     query = f"SELECT * from users where id={user_id}"
     user = db.search(query=query)
     if user:
-        id, name, country, username, email, hash = user[0]
+        id, name, surname, country, username, email, hash = user[0]
         user = {'username': name}
         return render_template('index.html', title='Home', user=user, login=True)
     return render_template('index.html', title='Home', login=True)
@@ -109,16 +122,23 @@ def login():
             login = f"SELECT * from users where email='{email}'"
             user = db.search(query=login)
             if user:
-                id, name, username, country, email, hash = user[0]
+                id, name, surname, country, username, email, hash = user[0]
                 if check_password(hashed_password=hash, user_password=password):
                     print("password is correct")
                     token = jwt.encode({'user_id': id, 'exp': datetime.utcnow() + timedelta(minutes=60)}, token_key, algorithm='HS256')
                     session['token'] = token
                     print("Token created.")
-                    return redirect('index')
-
-        return render_template('login.html')
-    return render_template('login.html', title='Sign In')
+                    return redirect('/')
+                else:
+                    print("password is incorrect")
+                    flash('Wrong password', 'danger')
+            else:
+                print("User does not exist.")
+                flash('User does not exist', 'danger')
+        else:
+            print("Please fill in all fields.")
+            flash('Please fill in all fields', 'danger')
+    return render_template('login.html', title='Sign In', messages=get_flashed_messages())
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -154,6 +174,13 @@ def signup():
                 return redirect(url_for('login'))
 
     return render_template('signup.html', title='Sign up')
+
+@app.route('/new-recipe', methods=['GET', 'POST'])
+@token_required
+def new_recipe():
+    if request.method == 'POST':
+
+    return render_template('new-recipe.html', title='New Recipe')
 
 @app.route('/logout')
 def logout():
